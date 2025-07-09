4d0ab6c1bd3d624dc86bd4d533da147e7e5d4704
Carlos-git-H <jhuaccachivilchez@gmail.com>
2025-07-09 12:17:32 -0600

    fix(controllers): api a los controllers

--- src/main/java/com/veterinaria/veterinaria_comoreyes/controller/AdminController.java
+++ src/main/java/com/veterinaria/veterinaria_comoreyes/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/panel-admin")
+@RequestMapping("/api/panel-admin")
 public class AdminController {
 
     private final ISpecieService specieService;
--- src/main/java/com/veterinaria/veterinaria_comoreyes/controller/ManagerController.java
+++ src/main/java/com/veterinaria/veterinaria_comoreyes/controller/ManagerController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/panel-manager")
+@RequestMapping("/api/panel-manager")
 public class ManagerController {
 
     private final ISpecieService specieService;
